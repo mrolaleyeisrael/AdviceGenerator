86f431f402573895a9a39e0d1fb5be2a67200ac7
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2022-03-22 23:54:23 +0100

    Vite create react & tailwind install

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className=' text-3xl text-green-500 bg-red-700'>
+      Independence Olaleye
+    </h1>
+  )
+}
+
+export default App
