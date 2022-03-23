fac625db6ec758ddf61146d5df32c9328b10a138
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2022-03-23 14:50:29 +0100

    Credit added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
 import Advice from './components/Advice'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className=' rounded-lg max-w-sm mx-4 sm:mx-auto bg-dark-gray-blue shadow-xl flex-col justify-center px-3 relative z-10 h-[300px] w-[400px]'>
+    <div>
+      <div className=' rounded-lg max-w-sm mx-4 sm:mx-auto bg-dark-gray-blue shadow-xl flex-col justify-center px-3 relative z-10 h-[300px] w-[400px]'>
 
-      <Advice />
-      
+        <Advice />
+
+      </div>
+
+      <div className=' text-white mt-20 w-2/3 sm:w-1/2 mx-auto'>
+        <Footer />
+      </div>
+
 
     </div>
+   
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <p>A React.js progressive web app by Israel Olaleye. Hosted on <a className=' text-neon-green  hover:drop-shadow-md' href="https://www.netlify.app/">Netlify</a></p>
+      <p>View source code on <a className=' text-neon-green' href="https://github.com/mrolaleyeisrael/AdviceGenerator">GitHub</a> .</p>
+    </div>
+  )
+}
+
+export default Footer
+
